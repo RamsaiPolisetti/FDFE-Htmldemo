022669ac002786fa814cad889e8ed92188f51dcd
2400031616 <2400031616@kluniversity.in>
2025-11-07 09:48:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  
+  return (
+    <div>
+      
+        Demonstration about html formateted
+        <h1>Welcome to klu</h1>
+        <h2>welcome to klu</h2>
+        <h3>Welcome to klu</h3>
+      <p>The HTML  tag is a fundamental element used to define a paragraph of text within a web page. It is a block-level element, meaning it typically starts on a new line and occupies the full available width of its parent container.The HTML  tag is a fundamental element used to define a paragraph of text within a web page. It is a block-level element, meaning it typically starts on a new line and occupies the full available width of its parent container.</p>
+      <a href="">welcome to klu</a> <br/>
+      <a href="">welcome to klu -erp</a> <br/>
+      <a href="">welcome to w3 school</a> <br/>
+    </div>
+       
+  )
+}
+
+export default App
